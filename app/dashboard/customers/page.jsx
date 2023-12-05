099697c72e1c8bbe32cbb61a0a6df4806ab49748
emondewan00 <dewanmohammademon@gmail.com>
2023-12-05 23:59:26 +0600

    nextjs-dashboard

---
+++ app/dashboard/customers/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>hello coustomers</div>;
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>hello invoices</div>;
+};
+
+export default page;
