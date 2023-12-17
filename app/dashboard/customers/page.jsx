ea12c71f2c55b5069a28c61c067cef96ec94c023
emondewan00 <dewanmohammademon@gmail.com>
2023-12-17 23:43:54 +0600

    completed

--- app/dashboard/customers/page.jsx
+++ app/dashboard/customers/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   return <div>hello coustomers</div>;
 };
 
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -21,7 +21,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  
   return (
     <>
       {links.map((link) => {
@@ -30,7 +29,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${pathname=== link.href && "text-blue-600 bg-sky-100"}`}
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
+              pathname === link.href && 'bg-sky-100 text-blue-600'
+            }`}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
